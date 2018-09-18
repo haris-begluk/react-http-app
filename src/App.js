fcbc5ab9cc9d0e0a2d3b8c90689d7130ea4edeb6
haris-begluk <harisbegluk@gmail.com>
2018-09-18 10:58:24 +0200

    HTTP: Extracting a reusable http service

--- src/App.js
+++ src/App.js
@@ -1,39 +1,25 @@
 import React, { Component } from "react";
-import axios from "axios";
 import "./App.css";
-//axios.interceptors.response.use(success, error);
-//we can log success errors or changes if we want
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error occured!");
-  }
-
-  return Promise.reject(error);
-});
+import http from "./services/httpService";
 const apiEndpoint = "http://jsonplaceholder.typicode.com/posts";
 class App extends Component {
   state = {
     posts: []
   };
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
   handleAdd = async () => {
     const obj = { title: "title a", body: "body a" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
   };
 
   handleUpdate = async post => {
     post.title = "Updated";
-    await axios.put(apiEndpoint + "/" + post.id, post);
+    await http.put(apiEndpoint + "/" + post.id, post);
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
     posts[index] = { ...post };
@@ -46,8 +32,8 @@ class App extends Component {
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
     try {
-      await axios.delete(apiEndpoint + "/" + post.id);
-      // to testit errors use  await axios.delete(apiEndpoint + "/5325" + post.id);
+      await http.delete(apiEndpoint + "/" + post.id);
+      // to testit errors use  await http.delete(apiEndpoint + "/5325" + post.id);
     } catch (ex) {
       if (ex.response && ex.response.status === 404)
         alert("This post has already been deleted!");
